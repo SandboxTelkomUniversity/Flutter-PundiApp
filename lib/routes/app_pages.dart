a5e5cdfb0c7e27027336bf81ce3a84d1c8f7e9d8
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2022-06-18 19:14:06 +0700

    Create onboarding

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,16 +1,21 @@
 import 'package:get/get.dart';
+import 'package:pundi_apps/ui/onboarding/onboarding_page.dart';
 
 import '../ui/main/main_page.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
-  static const initial = Routes.main;
+  static const initial = Routes.onboarding;
 
   static final routes = [
     GetPage(
       name: Routes.main,
       page: () => const MainPage(),
+    ),
+    GetPage(
+      name: Routes.onboarding,
+      page: () => const OnboardingPage(),
     ),
   ];
 }

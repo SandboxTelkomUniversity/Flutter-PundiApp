08c1c7b5d37414ea6082153c530a48b7dca8826a
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2022-06-19 04:39:21 +0700

    Add send page

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,16 +1,21 @@
 import 'package:get/get.dart';
+import 'package:pundi_apps/ui/send/send_page.dart';
 
 import '../ui/main/main_page.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
-  static const initial = Routes.main;
+  static const initial = Routes.send;
 
   static final routes = [
     GetPage(
       name: Routes.main,
       page: () => const MainPage(),
+    ),
+    GetPage(
+      name: Routes.send,
+      page: () => const SendPage(),
     ),
   ];
 }

e922729808477f3d1ce79c7e779b5a7712587ee2
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2022-06-18 14:02:18 +0700

    Revert commit

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,26 +1,16 @@
 import 'package:get/get.dart';
-import 'package:pundi_apps/ui/splashscreen/splash_screen_page.dart';
-import 'package:pundi_apps/ui/login/login_page.dart';
 
 import '../ui/main/main_page.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
-  static const initial = Routes.splash;
+  static const initial = Routes.main;
 
   static final routes = [
     GetPage(
-      name: Routes.splash,
-      page: () => const SplashScreenPage(),
-    ),
-    GetPage(
       name: Routes.main,
       page: () => const MainPage(),
-    ),
-    GetPage(
-      name: Routes.login,
-      page: () => const LoginPage(),
     ),
   ];
 }
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,7 +1,5 @@
 part of 'app_pages.dart';
 
 abstract class Routes {
-  static const String splash = '/splash';
   static const String main = '/main';
-  static const String login = '/login';
 }

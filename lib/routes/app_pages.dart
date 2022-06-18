5622494f547d8dd94b2cba240b743f7b0c40c319
Muhammad Hanan Rafif Farizta <hananfariztaa@gmail.com>
2022-06-18 20:34:57 +0700

    update home page

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,13 +1,12 @@
 import 'package:get/get.dart';
 import 'package:pundi_apps/ui/home/home_page.dart';
-import 'package:pundi_apps/ui/login/login_page.dart';
 
 import '../ui/main/main_page.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
-  static const initial = Routes.home_page;
+  static const initial = Routes.home;
 
   static final routes = [
     GetPage(
@@ -15,11 +14,7 @@ class AppPages {
       page: () => const MainPage(),
     ),
     GetPage(
-      name: Routes.login,
-      page: () => const LoginPage(),
-    ),
-    GetPage(
-      name: Routes.home_page,
+      name: Routes.home,
       page: () => const HomePage(),
     ),
   ];
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,6 +2,5 @@ part of 'app_pages.dart';
 
 abstract class Routes {
   static const String main = '/main';
-  static const String login = '/login';
-  static const String home_page = '/home';
+  static const String home = '/home';
 }

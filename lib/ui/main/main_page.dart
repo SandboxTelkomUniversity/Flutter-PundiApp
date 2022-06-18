45a42828e88aeba0778d036a1a478bf990afd2c5
Muhammad Hanan Rafif Farizta <hananfariztaa@gmail.com>
2022-06-19 03:25:58 +0700

    add bottom sheet

--- lib/ui/main/main_page.dart
+++ lib/ui/main/main_page.dart
@@ -37,7 +37,176 @@ class MainPage extends StatelessWidget {
         () => FloatingActionButton(
           backgroundColor:
               mainController.dontChange.value ? null : AppColor.halloweenOrange,
-          onPressed: () {},
+          onPressed: () {
+            showModalBottomSheet(
+                backgroundColor: Colors.transparent,
+                context: context,
+                builder: (context) {
+                  return Container(
+                    height: 350,
+                    decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.vertical(top: Radius.circular(20)),
+                        color: AppColor.white),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 12),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Center(
+                            child: Container(
+                              width: 55,
+                              height: 6,
+                              decoration: BoxDecoration(
+                                color: AppColor.orange,
+                                borderRadius: BorderRadius.circular(26),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 28),
+                          Text(
+                            'Mau kirim kemana ?',
+                            style: AppText.textMedium.copyWith(
+                              fontWeight: AppText.semiBold,
+                              fontSize: 14,
+                              color: AppColor.black,
+                            ),
+                          ),
+                          SizedBox(height: 32),
+                          Container(
+                              height: 75,
+                              decoration: BoxDecoration(
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.3),
+                                      spreadRadius: 1,
+                                      blurRadius: 2,
+                                      offset: Offset(
+                                          0, 0), // changes position of shadow
+                                    ),
+                                  ],
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: AppColor.white),
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 24, bottom: 20, top: 20),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Align(
+                                          alignment: Alignment.topCenter,
+                                          child: SvgPicture.asset(
+                                            'assets/vector/ic_transaction.svg',
+                                            height: 32,
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          width: 10,
+                                        ),
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              'Kirim antar E-Wallet',
+                                              style: AppText.textNormal
+                                                  .copyWith(
+                                                      color: AppColor.black,
+                                                      fontWeight:
+                                                          AppText.medium,
+                                                      fontSize: 12),
+                                            ),
+                                            Text(
+                                              'Pindahkan saldo ke semua E-Wallet, gratis!',
+                                              style: AppText.textNormal
+                                                  .copyWith(
+                                                      color: AppColor.black
+                                                          .withOpacity(0.5),
+                                                      fontWeight:
+                                                          AppText.regular,
+                                                      fontSize: 10),
+                                            ),
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              )),
+                          SizedBox(height: 20),
+                          Container(
+                              height: 75,
+                              decoration: BoxDecoration(
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.3),
+                                      spreadRadius: 1,
+                                      blurRadius: 2,
+                                      offset: Offset(
+                                          0, 0), // changes position of shadow
+                                    ),
+                                  ],
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: AppColor.white),
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 24, bottom: 20, top: 20),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Align(
+                                          alignment: Alignment.topCenter,
+                                          child: SvgPicture.asset(
+                                            'assets/vector/ic_bank.svg',
+                                            height: 32,
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          width: 10,
+                                        ),
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              'Kirim E-Wallet ke Bank',
+                                              style: AppText.textNormal
+                                                  .copyWith(
+                                                      color: AppColor.black,
+                                                      fontWeight:
+                                                          AppText.medium,
+                                                      fontSize: 12),
+                                            ),
+                                            Text(
+                                              'Transfer ke lebih dari 100 bank di Indonesia',
+                                              style: AppText.textNormal
+                                                  .copyWith(
+                                                      color: AppColor.black
+                                                          .withOpacity(0.5),
+                                                      fontWeight:
+                                                          AppText.regular,
+                                                      fontSize: 10),
+                                            ),
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              )),
+                        ],
+                      ),
+                    ),
+                  );
+                });
+          },
           tooltip: "Transfer",
           elevation: 4.0,
           child: Container(

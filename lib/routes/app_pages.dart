d8770e8ec26d7d34c2388944fd6ec665eddab212
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2022-06-19 11:46:16 +0700

    Add wait page

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,16 +1,21 @@
 import 'package:get/get.dart';
+import 'package:pundi_apps/ui/wait/wait_page.dart';
 
 import '../ui/main/main_page.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
-  static const initial = Routes.main;
+  static const initial = Routes.wait;
 
   static final routes = [
     GetPage(
       name: Routes.main,
       page: () => const MainPage(),
+    ),
+    GetPage(
+      name: Routes.wait,
+      page: () => const WaitPage(),
     ),
   ];
 }
